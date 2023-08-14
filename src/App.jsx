fb18793235aaf13b25fb1d3b05681dde89fef2b3
noir210397 <77852592+noir210397@users.noreply.github.com>
2023-08-14 03:04:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(()=>{
+    console.log(import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID)
+  })
   return (
     <>
       <p className="bg-red-500">hello world</p>
