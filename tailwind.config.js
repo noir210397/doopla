dff3155134dc094b0a205a4f9880ce3e7ad7686b
noir210397 <77852592+noir210397@users.noreply.github.com>
2023-08-14 02:18:51 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      pacifico: ["Pacifico", "sans-serif"],
+      lora: ["Lora", "sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
